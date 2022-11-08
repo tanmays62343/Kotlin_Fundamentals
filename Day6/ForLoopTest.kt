78cc6c3b5fe717a94d3d1949206fb16ff65214ea
Tanmay Deopurkar <114358603+tanmays62343@users.noreply.github.com>
2022-11-08 23:57:05 +0530

    For Loop in Kotlin

---
+++ Day6/ForLoopTest.kt
@@ -0,0 +1,18 @@
+fun main (){
+
+    for (i in 1..5){
+        println("HELLO")
+    }
+    println()
+    for (i in 1 until 5){
+        println("WORLD")
+    }
+    println()
+    for(i in 5 downTo 1){
+        println("XD")
+    }
+    println("______________________________________")
+    for(i in 10 downTo (1) step(2)){
+        print("$i ")
+    }
+}
