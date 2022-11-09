ca3a4a5eb5efdb550f9c1bddcc2c65d297a289fb
Tanmay Deopurkar <114358603+tanmays62343@users.noreply.github.com>
2022-11-09 22:01:19 +0530

    Functions in Kotlin

---
+++ Day7/Funtions.kt
@@ -0,0 +1,19 @@
+fun main(){
+    println(addition(10,20))
+    println(subs(20,10))
+    println(multiplication(10,10))
+    println(division(100,10))
+
+}
+fun addition(num1 : Int ,num2 : Int) :Int {
+    return num1+num2
+}
+fun subs(num1: Int , num2: Int) :Int {
+    return num1-num2
+}
+fun multiplication(num1: Int , num2: Int) :Int {
+    return num1*num2;
+}
+fun division(num1: Int,num2: Int) :Int{
+    return num1/num2;
+}
